a991f53873818442b00e0f7e9af695423db50a89
OlafVanHuusen <lennard.clicque@fau.de>
2025-12-02 23:37:10 +0100

    day 2 part 2

--- Day 2/2.rb
+++ Day 2/2.rb
@@ -1,9 +1,9 @@
 require 'aoc_utils'
 
-@facts_dict = {2 => [1], 3 => [1], 4 => [2], 5 => [5], 6 => [3,2], 7 => [1], 8 => [4], 9 => [3], 10 => [5,2]}
+@facts_dict = {2 => [1], 3 => [1], 4 => [2], 5 => [5], 6 => [3,2], 7 => [1], 8 => [4], 9 => [3], 10 => [5,2], 11 => [1], 12 => [6]}
 
 def main
-  ranges_strings = AocUtils.read_strings('Day 2/testinput.txt').flatten
+  ranges_strings = AocUtils.read_strings('Day 2/input.txt').flatten
   ranges = ranges_strings.map { |range_string| range_string_to_array(range_string) }
   ranges = ranges.reduce([]) do |acc, range|
     acc + split_range_into_ranges_with_same_digit_count(range)
@@ -18,25 +18,37 @@ end
 
 def find_all_invalid_ids_part2(range)
   digit_count = range[0].to_s.length
+  if digit_count < 2
+    return []
+  end
+  if digit_count > 12
+    raise "not implemented for digit counts greater than 12"
+  end
   facts = @facts_dict[digit_count]
   invalid_ids = []
   (0..(facts.length - 1)).each do |i|
     first_x = facts[i]
     left_side_min = range[0].to_s[0, first_x].to_i
     left_side_max = range[1].to_s[0, first_x].to_i
+    (left_side_min..left_side_max).each do |left_side|
+      left_side_string = left_side.to_s
+      invalid_id = (left_side_string * (digit_count/first_x)).to_i
+      invalid_ids << invalid_id if invalid_id >= range[0] && invalid_id <= range[1]
+    end
   end
+  invalid_ids.uniq
 end
 
 def split_range_into_ranges_with_same_digit_count(range)
-  if range[0].to_s.length == range[1].to_s.length
-    [range]
+  min_length = range[0].to_s.length
+  max_length = range[1].to_s.length
+  if min_length == max_length
+    return [range]
   end
-  if (range[0].to_s.length - range[1].to_s.length).abs != 1
+  if (range[0].to_s.length - range[1].to_s.length).abs > 1
     raise "not implemented for ranges with more than 1 digit count difference"
   end
   result = []
-  min_length = range[0].to_s.length
-  max_length = range[1].to_s.length
   first_range_end = (10**min_length) - 1
   second_range_start = 10**min_length
   result << [range[0], first_range_end]
