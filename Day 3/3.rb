b1ff2784afcefdc270e4e3e0d7c4545b7bc6e6c1
OlafVanHuusen <lennard.clicque@fau.de>
2025-12-03 16:03:37 +0100

    cleaned up day 3

--- Day 3/3.rb
+++ Day 3/3.rb
@@ -2,20 +2,18 @@ require 'aoc_utils'
 
 def main
   time_start = Time.now
-  banks = AocUtils.read_chars('Day 3/input.txt')
-  banks = char_arr_to_int_arr(banks)
+  banks = AocUtils.read_chars('Day 3/testinput.txt')
+  banks.map! { |num| num.map(&:to_i) }
   # === Part 1 ===
-  print banks.reduce(0) { |sum, bank| sum + maximum_joltage(bank, 2) }
+  print "Result Part 1: ", banks.reduce(0) { |sum, bank| sum + maximum_joltage(bank, 2) }, "\n"
   # === Part 2 ===
-  print banks.reduce(0) { |sum, bank| sum + maximum_joltage_part2(bank, 12) }
+  print "Result Part 2: ", banks.reduce(0) { |sum, bank| sum + maximum_joltage(bank, 12) }, "\n"
   # === End ===
   time_end = Time.now
   puts "\nExecution Time: #{(time_end - time_start) * 1000} ms"
 end
 
-# === Part 2 ===
-
-def maximum_joltage_part2(bank, n_batteries)
+def maximum_joltage(bank, n_batteries)
   values = []
   n_batteries.downto(1) do |i|
     leftmost_max_index_i = bank[0..-i].find_index(bank[0..-i].max)
@@ -23,18 +21,6 @@ def maximum_joltage_part2(bank, n_batteries)
     bank = bank[(leftmost_max_index_i + 1)..-1]
   end
   values.reduce(""){|string, element| string + element.to_s}.to_i
-end
-
-# === Part 1 ===
-def maximum_joltage(bank)
-  leftmost_max_index = bank[0..-2].find_index(bank[0..-2].max)
-  second_max_index = bank[leftmost_max_index + 1..-1].find_index(bank[leftmost_max_index + 1..-1].max)
-  second_max_index += leftmost_max_index + 1
-  (bank[leftmost_max_index].to_s + bank[second_max_index].to_s).to_i
-end
-
-def char_arr_to_int_arr(arr)
-  arr.map { |num| num.map(&:to_i) }
 end
 
 main
